ad9b72b4eeae48ac0dd2941365b1f0bfef78521d
Aaron Baker <baker.1833@osu.edu>
2013-07-16 17:36:22 -0400

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Nottwitter::Application.routes.draw do
+
+  root 'users#new'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
