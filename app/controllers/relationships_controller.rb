16faa579e1064d63870f606d0ff8bd5058cccc10
Aaron Baker <baker.1833@osu.edu>
2013-07-22 10:50:57 -0400

    notification created/destroyed when following/unfollowing a user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,6 +2,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:id])
     current_user.follow(@user)
+    Notification.create(user_id: @user.id, content: "#{current_user.username} is now following you!")
     respond_to do |format|
       format.js
     end
@@ -10,6 +11,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     current_user.unfollow(@user)
+    Notification.find_by(user_id: @user.id, content: "#{current_user.username} is now following you!").destroy
     respond_to do |format|
       format.js
     end
