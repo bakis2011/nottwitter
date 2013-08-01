0d571eb20f8595ad556de7ad4bd338e9273d2622
Aaron Baker <aaron.baker@neo.com>
2013-08-01 10:42:05 -0400

    find or create notification instead of creating a notification every time

--- app/helpers/nottweets_helper.rb
+++ app/helpers/nottweets_helper.rb
@@ -1,15 +1,16 @@
 module NottweetsHelper
   REGEXP = /((@)[\w\-]+|(#)[\w\-]+)/
-  def linkify_keywords(content)
-    # TODO Change this, I don't like this.
-    if content
-      return content.gsub(REGEXP) do |match|
-        case $~.captures.compact[1]
-          when "@" then
-            id = User.find_by(username: match[1..-1]).try(:id)
-            id ? "<a href='/users/#{id}'>#{match}</a>" : match
-          when "#" then "<a href='/search/#{match[1..-1]}'>#{match}</a>"
-        end
+  def linkify_keywords(nottweet)
+    return nottweet.content.gsub(REGEXP) do |match|
+      case $~.captures.compact[1]
+        when "@" then
+          if user = User.find_by(username: match[1..-1])
+            Notification.find_or_create_by(author: current_user, user: user, action: "mention", nottweet: nottweet)
+            "<a href='/users/#{user.id}'>#{match}</a>"
+          else
+            match
+          end
+        when "#" then "<a href='nottweets/search/#{match[1..-1]}'>#{match}</a>"
       end
     end
   end
