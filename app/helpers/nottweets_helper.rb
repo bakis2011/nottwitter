501970ca86628763b188c0e2e5a5375d4dfff2fc
Aaron Baker <aaron.baker@neo.com>
2013-08-01 10:47:32 -0400

    support for notifications without borks

--- app/helpers/nottweets_helper.rb
+++ app/helpers/nottweets_helper.rb
@@ -1,16 +1,18 @@
 module NottweetsHelper
   REGEXP = /((@)[\w\-]+|(#)[\w\-]+)/
   def linkify_keywords(nottweet)
-    return nottweet.content.gsub(REGEXP) do |match|
-      case $~.captures.compact[1]
-        when "@" then
-          if user = User.find_by(username: match[1..-1])
-            Notification.find_or_create_by(author: current_user, user: user, action: "mention", nottweet: nottweet)
-            "<a href='/users/#{user.id}'>#{match}</a>"
-          else
-            match
-          end
-        when "#" then "<a href='nottweets/search/#{match[1..-1]}'>#{match}</a>"
+    if nottweet
+      return nottweet.content.gsub(REGEXP) do |match|
+        case $~.captures.compact[1]
+          when "@" then
+            if user = User.find_by(username: match[1..-1])
+              Notification.find_or_create_by(author: current_user, user: user, action: "mention", nottweet: nottweet)
+              "<a href='/users/#{user.id}'>#{match}</a>"
+            else
+              match
+            end
+          when "#" then "<a href='nottweets/search/#{match[1..-1]}'>#{match}</a>"
+        end
       end
     end
   end
