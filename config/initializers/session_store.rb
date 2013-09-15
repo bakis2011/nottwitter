cbb4e970afe5bdad7b9fd309f9b5bc97ac5a5f12
Aaron Baker <aaron.baker@neo.com>
2013-09-15 17:26:42 -0400

    some session config changes, maybe this will fix something

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Nottwitter::Application.config.session_store :active_record_store
+Nottwitter::Application.config.session_store :active_record_store, {
+  :key => '_session_id',
+  :path => '/',
+  :domain => nil,
+  :epxire_after => nil,
+}
