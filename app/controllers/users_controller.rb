aaa4b47847d4ed5503732f3cd91ad068495865b3
Aaron Baker <aaron.baker@neo.com>
2013-09-16 16:11:58 -0400

    serve user information via json for app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.order('created_at DESC')
+    respond_to do |format|
+      format.html
+      format.json { render json: @users}
+    end
   end
 
   def new
