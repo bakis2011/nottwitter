299d27696e0af52c036106b9a12523b77e0b71d4
Aaron Baker <aaron.baker@neo.com>
2013-10-01 16:59:23 -0400

    modify notification api endpoint to respond to limits and date parameters

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -1,11 +1,16 @@
 class Api::NotificationsController < ApiController
   before_filter :require_valid_username
   before_filter :require_limit
+  before_filter :require_date
 
   def index
-    if valid_username_present? && limit_present?
-      user = User.find_by(username: params[:username])
-      render json: user.notifications.limit(params[:limit]).order('created_at DESC')
+    user = User.find_by(username: params[:username])
+    if params[:older_than].present?
+      older_than = Time.parse(params[:older_than])
+      render json: user.notifications.limit(params[:limit]).where('created_at < ?', older_than).order('created_at DESC')
+    else
+      since = Time.parse(params[:since])
+      render json: user.notifications.limit(params[:limit]).where('created_at > ?', since).order('created_at DESC')
     end
   end
 end
