ca7fa43f1c1bf1154f07e6aad9df64b4dcc40790
Aaron Baker <aaron.baker@neo.com>
2013-10-02 10:17:27 -0400

    more robust user borks method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,7 @@ class Api::UsersController < ApiController
   before_filter :require_token, only: [:add_token]
   before_filter :require_password, only: [:authenticate]
   before_filter :require_limit, only: [:user_borks]
+  before_filter :require_date, only: [:user_borks]
 
   def index
     render json: User.order('created_at DESC')
@@ -36,6 +37,13 @@ class Api::UsersController < ApiController
   end
 
   def user_borks
-    render json: Bork.where(user_id: User.find_by(username: params[:username])).limit(params[:limit])
+    @user = User.find_by(username: params[:username])
+    if params[:older_than].present?
+      older_than = Time.parse(params[:older_than])
+      render json: Bork.where(user_id: @user.id).limit(params[:limit]).where('created_at < ?', older_than).order('created_at DESC')
+    else
+      since = Time.parse(params[:since])
+      render json: Bork.where(user_id: @user.id).limit(params[:limit]).where('created_at > ?', since).order('created_at DESC')
+    end
   end
 end
