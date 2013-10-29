f373e587cede1ee42ce79f138f3bbefdcca641cf
Aaron Baker <aaron.baker@neo.com>
2013-10-29 09:51:18 -0400

    render js first before sending push notification so it isnt so damn slow to toggle action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,19 @@
 class FavoritesController < ApplicationController
   def create
     @bork = Bork.find(params[:id])
-    current_user.favorite(@bork)
-    Notification.create(author: current_user, bork: @bork, user: @bork.user, action: "favorite")
     respond_to do |format|
       format.js
     end
+    current_user.favorite(@bork)
+    Notification.create(author: current_user, bork: @bork, user: @bork.user, action: "favorite")
   end
 
   def destroy
     @bork = Bork.find(params[:id])
-    current_user.unfavorite(@bork)
-    Notification.find_by(author: current_user, bork: @bork, user: @bork.user, action: "favorite").destroy
     respond_to do |format|
       format.js
     end
+    current_user.unfavorite(@bork)
+    Notification.find_by(author: current_user, bork: @bork, user: @bork.user, action: "favorite").destroy
   end
 end
