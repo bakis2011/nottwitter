28921d51986c43c9901f304ead79146bd146e1fd
Edgecase Pairing <bakis2011@gmail.com>
2015-06-22 19:58:19 -0700

    remove broken push notifications for now

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,7 +3,6 @@ class Notification < ActiveRecord::Base
   belongs_to :bork
   belongs_to :author, class_name: "User"
   before_create :create_api_content
-  after_create :push_notification
 
   def mark_as_read
     self.read = true
@@ -22,16 +21,16 @@ class Notification < ActiveRecord::Base
     end
   end
 
-  def push_notification
-    Apns.where(user_id: user_id).each do |apns|
-      notification = Houston::Notification.new(device: apns.token)
-      notification.badge = 1
-      if (action == "favorite")
-        notification.alert = "#{author.username} favorited your bork \"#{bork.content}\""
-      else
-        notification.alert = "#{author.username} mentioned you in his bork \"#{bork.content}\""
-      end
-      APN.push(notification)
-    end
-  end
+  # def push_notification
+  #   Apns.where(user_id: user_id).each do |apns|
+  #     notification = Houston::Notification.new(device: apns.token)
+  #     notification.badge = 1
+  #     if (action == "favorite")
+  #       notification.alert = "#{author.username} favorited your bork \"#{bork.content}\""
+  #     else
+  #       notification.alert = "#{author.username} mentioned you in his bork \"#{bork.content}\""
+  #     end
+  #     APN.push(notification)
+  #   end
+  # end
 end
